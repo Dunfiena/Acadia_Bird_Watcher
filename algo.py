b286746fd56046373e821b2133043c0da1f89b08
dunfi <dunfiena@gmail.com>
2024-06-29 11:43:59 -0300

    June 21st doubled object detection and removal working

--- algo.py
+++ algo.py
@@ -2,18 +2,10 @@ import Bird
 
 
 def clean_slate(bird):
-    bird_copy = bird
-    birds_return = []
     for check in bird:
-        double_flag = False
-        for b in bird_copy:
-            if b.getX() == check.getX() and b.getY() == check.getY() and b.getAge() == check.getAge():
-                if not double_flag:
-                    birds_return.append(check)
-                    bird_copy.remove(b)
-                    double_flag = True
-                else:
-                    bird.remove(b)
+        for b in bird:
+            if (b.getX() == check.getX() and b.getY() == check.getY()
+                    and b.getAge() == check.getAge() and b.getId() != check.getId()):
+                bird.remove(b)
 
-
-    return birds_return
+    return bird
